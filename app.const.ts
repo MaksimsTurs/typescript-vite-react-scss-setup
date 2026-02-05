271a48117c1b5557b800f30fe35fa850c85b3b0a
Maksims Turs <maksims07turs@gmail.com>
2026-02-05 23:50:07 +0100

    Changed: removed unused configuration constants.

--- app.const.ts
+++ app.const.ts
@@ -4,6 +4,4 @@ export const DIRNAME: string                     = __dirname;
 
 export const APP_TYPE: AppType                   = "spa";
 
-export const ASSETS_INCLUDE_EXTENSIONS: string[] = ['**/*.png', '**/*.webp', '**/*.jpg', '**/*.jpeg'];
-
-export const IMG_REGEXP: RegExp                  = /\.(jpeg|jpg|png|webp|gif)/i;
+export const ASSETS_INCLUDE_EXTENSIONS: string[] = ["**/*.png", "**/*.webp", "**/*.jpg", "**/*.jpeg"];
