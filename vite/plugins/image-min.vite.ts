f5072838fb0b8c127d312daf9275493853496d4f
Maksims Turs <maksims07turs@gmail.com>
2026-02-06 00:11:29 +0100

    Fixed: unplugin-imagemin does not create cache folder in src anymore.

--- vite/plugins/image-min.vite.ts
+++ vite/plugins/image-min.vite.ts
@@ -2,6 +2,8 @@ import type { Plugin } from "vite";
 
 import imagemin from "unplugin-imagemin/vite";
 
+import resolve from "../utils/resolve.util";
+
 export default function(): Plugin<any> {
-  return imagemin();
+  return imagemin({ cacheLocation: resolve("node_modules\\unplugin-imagemin-cache") });
 };
