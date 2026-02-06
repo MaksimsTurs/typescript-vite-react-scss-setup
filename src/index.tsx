3928cc7b60d150fe0e06d4928c05ad6c58787b9f
Maksims Turs <maksims07turs@gmail.com>
2026-02-06 16:23:08 +0100

    Changed: removed examples in index.tsx.

--- src/index.tsx
+++ src/index.tsx
@@ -2,31 +2,18 @@ import "@scss/root.scss";
 
 import type { ReactNode } from "react";
 
+import { Fragment } from "react";
+
 import { createRoot } from "react-dom/client";
 
 import ErrorBoundary from "./components/Error-Boundary/Error-Boundary.component";
-import { Routes, initRouteComponents, useParams, usePath } from "./hooks/use-react-router/use-react-router.hook";
+import { Routes, initRouteComponents } from "./hooks/use-react-router/use-react-router.hook";
 import { ReactStorageProvider } from "./hooks/use-react-storage/use-storage.hook";
 
 export const { Route, Link } = initRouteComponents<string>();
 
 function App(): ReactNode {
-  const params = useParams();
-  const path = usePath();
-
-  console.log(params, path);
-
-  return(
-    <>
-      <Route path="/" children={
-        <Link href="/2599" children="User"/> 
-      }/>
-      <Route path="/:id" children={
-        <Link href="/" children={`User: ${params.id}`}/>
-      }/>
-      {/* children */}
-    </>
-  );
+  return <Fragment>{/* children */}</Fragment>;
 };
 
 createRoot(document.body)
