65cf1ed6403862367bb5da341e9fb397ba604e84
Maksims Turs <maksims07turs@gmail.com>
2026-02-07 00:12:19 +0100

    Fixed: scss compiling.

--- vite/options/css.vite.ts
+++ vite/options/css.vite.ts
@@ -6,12 +6,6 @@ export default function(): CSSOptions {
   return {
     transformer:            "postcss",
     devSourcemap:           true,
-    preprocessorOptions: {
-      scss: {
-        style: "compressed",
-        syntax: "css",
-      }
-    },
     preprocessorMaxWorkers: true,
     postcss:                resolve("postcss.config.ts"),
 		modules: {
